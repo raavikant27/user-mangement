8cae9b43b55ecba5f6ccda4f2a5170ec00e5de8e
Ravikant singh <sravikant302@gmail.com>
2025-03-30 19:32:36 +0530

    first commit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const session = require('express-session');
 const connectDB = require('./server/config/db');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 // Connect to Database  
 connectDB();
--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose');
-const connectDB = async()=> {
+
+const connectDB = async () => {
   try {
     mongoose.set('strictQuery', false);
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    // Replace the process.env.MONGODB_URI with your MongoDB connection URI directly
+    const conn = await mongoose.connect('mongodb+srv://singhravisingh178:K0Sz0mpGxl3Chgx3@cluster0.tklj2uu.mongodb.net/', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`Database Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log(error);
+    console.log(`Error: ${error.message}`);
+    process.exit(1); // Exit process with failure
   }
-}
-
-
+};
 
 module.exports = connectDB;
-
